7685cda6c4193b1aa96f8cfd8808d084b51c08d2
Jack Mumford <jack@cmumford.com>
2025-04-23 11:48:56 -0700

    Started tests changed one thing which made it not work

--- Password.cpp
+++ Password.cpp
@@ -1,29 +1,54 @@
 #include "Password.h"
 #include <string>
+#include <cassert>
+#include <ctype.h>
 
 using std::string;
 
 /*
-  The function receives a string counts how many times the same character 
+  The function receives a string counts how many times the same character
   occurs at the beginning of the string, before any other characters (or the
   end of the string). The function is case-sensitive so 'Z' is different than
   'z' and any ASCII characters are allowed.
 */
-int Password::count_leading_characters(string phrase){
+int Password::count_leading_characters(string phrase)
+{
+  if (!phrase.empty())
+  {
+    return false;
+  }
   int repetition = 1;
   int index = 0;
-  while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
+  while (index < phrase.length() - 1 && phrase[index] == phrase[index + 1])
+  {
     repetition++;
     index++;
   }
   return repetition;
 }
 
-
 /*
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
+bool Password::has_mixed_case(string pass)
+{
+  if (!pass.empty())
+  {
+    return false;
+  }
+  bool has_lower = false;
+  bool has_upper = false;
+
+  for (char c : pass)
+  {
+    if (islower(c))
+      has_lower = true;
+    if (isupper(c))
+      has_upper = true;
+    if (has_lower && has_upper)
+      return true;
+  }
+
   return false;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -1,17 +1,17 @@
 /**
  * Unit Tests for Password class
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Password.h"
 
 class PasswordTest : public ::testing::Test
 {
-	protected:
-		PasswordTest(){} //constructor runs before each test
-		virtual ~PasswordTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+protected:
+	PasswordTest() {}		   // constructor runs before each test
+	virtual ~PasswordTest() {} // destructor cleans up after tests
+	virtual void SetUp() {}	   // sets up before each test (after constructor)
+	virtual void TearDown() {} // clean up after each test, (before destructor)
 };
 
 TEST(PasswordTest, single_letter_password)
@@ -19,4 +19,35 @@ TEST(PasswordTest, single_letter_password)
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+// Test for mixed case password
+TEST(PasswordTest, mixed_case_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("Aa");
+	ASSERT_TRUE(actual);
+}
+
+// Test for all upper case password
+TEST(PasswordTest, all_upper_case_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("AA");
+	ASSERT_FALSE(actual);
+}
+
+// Test for all lower case password
+TEST(PasswordTest, all_lower_case_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("aa");
+	ASSERT_FALSE(actual);
 }
